fc0d1a201dfcc898f7404bcd39daf2ffc6c78197
0xshoegazer <0xshoegazer@gmail.com>
2023-07-30 19:13:22 -0400

    liquidity

--- src/config/constants/exchange.ts
+++ src/config/constants/exchange.ts
@@ -21,7 +21,7 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
     bscTokens.usdc,
   ],
   [ChainId.BSC_TESTNET]: [bscTestnetTokens.wbnb, bscTestnetTokens.cake, bscTestnetTokens.busd],
-  [ChainId.BASE_GOERLI]: [baseGoerliTokens.wbnb],
+  [ChainId.BASE_GOERLI]: [baseGoerliTokens.wbnb, bscTestnetTokens.cake],
 }
 
 /**
@@ -45,12 +45,14 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 export const SUGGESTED_BASES: ChainTokenList = {
   [ChainId.MAINNET]: [bscTokens.busd, bscTokens.cake, bscTokens.btcb],
   [ChainId.BSC_TESTNET]: [bscTestnetTokens.wbnb, bscTestnetTokens.cake, bscTestnetTokens.busd],
+  [ChainId.BASE_GOERLI]: [bscTestnetTokens.wbnb, bscTestnetTokens.cake],
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   [ChainId.MAINNET]: [bscTokens.wbnb, bscTokens.dai, bscTokens.busd, bscTokens.usdt],
   [ChainId.BSC_TESTNET]: [bscTestnetTokens.wbnb, bscTestnetTokens.cake, bscTestnetTokens.busd],
+  [ChainId.BASE_GOERLI]: [bscTestnetTokens.wbnb, bscTestnetTokens.cake],
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
@@ -59,6 +61,7 @@ export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } 
     [bscTokens.busd, bscTokens.usdt],
     [bscTokens.dai, bscTokens.usdt],
   ],
+  [ChainId.BASE_GOERLI]: [[bscTestnetTokens.wbnb, bscTestnetTokens.cake]],
 }
 
 export const BIG_INT_ZERO = JSBI.BigInt(0)
