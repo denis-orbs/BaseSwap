3ebef9d50d6379fe65d0877380b7be35749558a0
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-07-31 10:46:22 -0400

    more

--- src/config/constants/pools.tsx
+++ src/config/constants/pools.tsx
@@ -25,7 +25,7 @@ export const vaultPoolConfig = {
     },
   },
   [VaultKey.CakeVault]: {
-    name: <Trans>Stake CAKE</Trans>,
+    name: <Trans>Stake BSWAP</Trans>,
     description: <Trans>Stake, Earn – And more!</Trans>,
     autoCompoundFrequency: 5000,
     gasLimit: 500000,
@@ -35,8 +35,8 @@ export const vaultPoolConfig = {
     },
   },
   [VaultKey.CakeFlexibleSideVault]: {
-    name: <Trans>Flexible CAKE</Trans>,
-    description: <Trans>Flexible staking on the side.</Trans>,
+    name: <Trans>Stake BSWAP</Trans>,
+    description: <Trans>Flexible staking.</Trans>,
     autoCompoundFrequency: 5000,
     gasLimit: 500000,
     tokenImage: {
