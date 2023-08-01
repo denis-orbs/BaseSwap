a0a3aecc31e1e974c9b22c767612b2a077029233
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-08-01 13:33:32 -0400

    1 for the money 2 for the show

--- packages/uikit/src/theme/colors.ts
+++ packages/uikit/src/theme/colors.ts
@@ -5,7 +5,7 @@ export const baseColors = {
   primary: "#0154FE",
   primaryBright: "#53DEE9",
   primaryDark: "#0098A1",
-  secondary: "#7645D9",
+  secondary: "#333",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -81,11 +81,11 @@ export const darkColors: Colors = {
     basedsexgrayflip: "linear-gradient(to top, #111, #333)",
 
     bubblegum: "linear-gradient(to bottom,  #020079 0%, #0154FD, #ccc 100%)",
-    inverseBubblegum: "linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)",
+    inverseBubblegum: "linear-gradient(139.73deg, #0154FD 0%, #ccc 100%)",
     cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
     blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
-    violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
-    violetAlt: "linear-gradient(180deg, #434575 0%, #66578D 100%)",
+    violet: "linear-gradient(180deg, #0154FD 0%, #ccc 100%)",
+    violetAlt: "linear-gradient(180deg, #0154FD 0%, #ccc 100%)",
     gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
   },
 };
