97ffb7bd2e9a0b4f6f7ef4de3bb4e4c0bc3f136e
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-08-01 16:48:34 -0400

    fu

--- src/components/Menu/Footer.tsx
+++ src/components/Menu/Footer.tsx
@@ -1,9 +1,9 @@
 import { memo } from 'react'
 import styled from 'styled-components'
-import { ButtonMenu, ButtonMenuItem, LinkExternal, Flex, Svg, Image, Button } from '@pancakeswap/uikit'
+import { ButtonMenu, ButtonMenuItem, LinkExternal, Flex, Svg, Image, Button, TwitterIcon, TelegramIcon, DiscordIcon } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
 import { EXCHANGE_DOCS_URLS } from 'config/constants'
-import { TwitterIcon, TelegramIcon, DiscordIcon } from '@pancakeswap/uikit'
+
 
 const Wrapper = memo(styled.div<{ $isSide: boolean }>`
   width: 100%;
