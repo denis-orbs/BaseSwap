6294d32372f6c83f78ef15458dacb4af57b4bcb6
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-08-03 22:28:05 -0400

    tvl

--- src/views/Home/components/TVL.tsx
+++ src/views/Home/components/TVL.tsx
@@ -6,21 +6,19 @@ const TVL = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+       
         const response = await fetch('https://api.llama.fi/tvl/BaseSwap');
         const data = await response.json();
-
+        // Round the data to the nearest hundred thousand
+        const roundedData = Math.round(data / 100000) * 100000;
+        
+        // Convert to millions for display
+        const displayValue = roundedData / 1000000;
         // round down the data to remove decimals
-        const roundedData = Math.floor(data);
+      
         console.log(roundedData)
 
-        const formatted = new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD',
-          // To prevent display of decimal places
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0,
-        }).format(roundedData);
-
+        const formatted = `${displayValue.toFixed(2)} million`; // append 'M' for million
         setFormattedValue(formatted);
       } catch (error) {
         console.error('Error fetching data:', error);
