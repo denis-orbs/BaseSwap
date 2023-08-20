e522ac727af9282bbde4003eb4e19da2c54de931
0xBriz <chaindevone@gmail.com>
2023-08-20 01:34:44 -0400

    more components

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -15,6 +15,26 @@ import usePrevious from 'hooks/usePrevious'
 // import { useSingleCallResult } from 'lib/hooks/multicall'
 // import { PositionPageUnsupportedContent } from 'pages/Pool/PositionPage'
 
+import { Text } from '@pancakeswap/uikit'
+import {
+  // useRangeHopCallbacks,
+  // useV3DerivedMintInfo,
+  // useV3MintActionHandlers,
+  useV3MintState,
+} from 'state/mint/v3/hooks'
+import styled, { useTheme } from 'styled-components'
+
+import { WRAPPED_NATIVE_CURRENCY } from 'config/constants/tokens'
+
+import MediumOnly, {
+  CurrencyDropdown,
+  DynamicSection,
+  ResponsiveTwoColumns,
+  ScrollablePage,
+  StyledInput,
+  Wrapper,
+} from './styled'
+
 function AddLiquidity() {
   return <div>Liq</div>
 }
