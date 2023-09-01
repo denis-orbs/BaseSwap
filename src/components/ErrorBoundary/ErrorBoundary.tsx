c4671340bc00e5f43d8d4c87276dbe3a46a59d70
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-09-01 12:44:08 -0400

    something wrong

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -4,6 +4,24 @@ import { useTranslation } from '@pancakeswap/localization'
 import { Button, Text, LogoIcon, Flex, IconButton, CopyIcon } from '@pancakeswap/uikit'
 import { copyText } from 'utils/copyText'
 import { useCallback } from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const float = keyframes`
+	0% {
+		transform: translatey(0px);
+	}
+	50% {
+		transform: translatey(10px);
+	}
+	100% {
+		transform: translatey(0px);
+	}
+`
+
+const FloatingImage = styled.img`
+  animation: ${float} 6s ease-in-out infinite;
+  transform: translate3d(0, 0, 0);
+`
 
 export default function ErrorBoundary({ children }) {
   const { t } = useTranslation()
@@ -17,9 +35,9 @@ export default function ErrorBoundary({ children }) {
         return (
           <Page>
             <Flex flexDirection="column" justifyContent="center" alignItems="center">
-              <LogoIcon width="64px" mb="8px" />
-              <Text mb="16px">{t('Oops, something wrong.')}</Text>
-              {eventId && (
+            <FloatingImage src="/images/pepetokens.png" alt="Pep" width="250px" />
+              <Text mb="16px">{t('Oops, something went wrong.')}</Text>
+              {/* {eventId && (
                 <Flex flexDirection="column" style={{ textAlign: 'center' }} mb="8px">
                   <Text>{t('Error Tracking Id')}</Text>
                   <Flex alignItems="center">
@@ -29,7 +47,7 @@ export default function ErrorBoundary({ children }) {
                     </IconButton>
                   </Flex>
                 </Flex>
-              )}
+              )} */}
               <Button onClick={handleOnClick}>{t('Click here to reset!')}</Button>
             </Flex>
           </Page>
