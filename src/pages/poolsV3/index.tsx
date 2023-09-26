975f4637e1f321895ffb82195b71cb1895b49147
TheHomelessCoder <mrcleantoiletdao@gmail.com>
2023-09-26 14:21:50 -0600

    text and spinner

--- src/pages/poolsV3/index.tsx
+++ src/pages/poolsV3/index.tsx
@@ -1,15 +1,31 @@
-import { Flex, Text } from '@pancakeswap/uikit'
+import { Flex, Text, Spinner } from '@pancakeswap/uikit'
+import styled from 'styled-components'
 import { TokenPairImage } from 'components/TokenImage'
 import { getTokenInstance } from 'config/constants/token-info'
 import useMerklRewards from 'lib/hooks/merkl-rewards/useMerklRewards'
 import { useRouter } from 'next/router'
 import Page from 'views/Page'
+import PageHeader from 'components/PageHeader'
 import {
   PoolCardAction,
   StyledPoolCard,
   StyledPoolCardInnerContainer,
 } from 'views/xFarms/components/NFTPoolCard/Styled'
 import { PoolCardActionProps } from 'views/xFarms/components/types'
+import TypeIt from 'typeit-react'
+import 'animate.css'
+
+const WelcomeTypeIt = styled(TypeIt)`
+  font-weight: 400;
+  color: #fff;
+  text-align: left;
+  margin-bottom: 12px;
+  text-transform: uppercase;
+  font-size: 40px;
+  @media (min-width: 768px) {
+    font-size: 68px;
+  }
+`
 
 export default function PoolV3({ table }: PoolCardActionProps) {
   const { data: merklData } = useMerklRewards()
@@ -39,9 +55,26 @@ export default function PoolV3({ table }: PoolCardActionProps) {
 
   return (
     <Page>
-      <Text>V3 POOLS</Text>
+      <PageHeader>
+          <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
+            <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
+              <WelcomeTypeIt
+                options={{
+                  cursorChar: ' ',
+                  cursorSpeed: 1000000,
+                  speed: 75,
+                }}
+                speed={10}
+                getBeforeInit={(instance) => {
+                  instance.type('V3 Pools', { speed: 5000 })
+                  return instance
+                }}
+              ></WelcomeTypeIt>
+            </Flex>
+          </Flex>
+        </PageHeader>
       <Flex>
-        {pools.map((p) => {
+        {pools?.length > 0 ? pools.map((p) => {
           return (
             <StyledPoolCard key={p.pool}>
               <StyledPoolCardInnerContainer>
@@ -61,7 +94,9 @@ export default function PoolV3({ table }: PoolCardActionProps) {
               </StyledPoolCardInnerContainer>
             </StyledPoolCard>
           )
-        })}
+        }):
+        <Spinner /> 
+      }
       </Flex>
     </Page>
   )
