887827b59a85335631e5a6db23847ccb4dec5deb
TheHomelessCoder <mrcleantoiletdao@gmail.com>
2023-09-27 17:03:40 -0600

    fix cors issue

--- apps/transfer/next.config.mjs
+++ apps/transfer/next.config.mjs
@@ -14,6 +14,19 @@ const nextConfig = {
       allowFutureImage: true,
     },
   },
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "https://api.angle.money/v1/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "https://baseswap.fi/:path*, http://localhost:3000" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]},
 }
 
 export default withTH(nextConfig)
