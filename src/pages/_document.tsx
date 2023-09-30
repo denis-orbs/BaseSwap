79e3999710c89d338ab3baa65910fcfa1d166408
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-09-29 23:22:53 -0400

    some dum shit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/logo.png" />
+          <link rel="apple-touch-icon" href="/newlogo.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
